780350803df074d71d1e306e17f9d878b62fcac9
Spen03 <jensenspencer00@gmail.com>
2023-10-13 13:57:17 -0400

    add homework11
    csc485\projects\tests\hw11\test_hw11.py
    +added files

--- csc485/tests/hw11/test_hw11.py
+++ csc485/tests/hw11/test_hw11.py
@@ -1,20 +1,102 @@
-from csc485.projects.hw11.getFruitName import get_formal_name
+from csc485.data.fruit import fruit_dict
+import pytest
 
+from csc485.projects.hw11.get_fruit_name import get_formal_name
+
+#from csc485.projects.hw10.fruit_query import is_it_a_fruit
+
+#from csc485.projects.hw11.get_fruit_name_v2 import get_formal_name_v2
+#can't seem to import the new file for testing. Maybe I should make a new test module?
 
 """
 test that when the function recieves a key it knows,
-it will return the formal name
+it will return the correctly associated value
 
-test that if it isn't a string,
-return that the function accepts fruits
+test that if it recieves an unknown key that it will return a key error
+these keys include:
+>strings
+>ints 
+>incorrectly formated strings
+>more than one argument
+>no argument
+>lists
+(could have tested methods but thought it unnecessary)
 """
 
 
 class TestHappyPath(object):
-    def test_apple(self):
-        assert get_formal_name('apple') == 'Malus domestica'
+    @pytest.mark.parametrize('good_key', [
+        ('apple', 'Malus domestica'),
+        ('banana', 'Musa acuminata'),
+        ('orange', 'Citrus × sinensis'),
+        ('strawberry', 'Fragaria × ananassa'),
+        ('grape', 'Vitis vinifera'),
+        ('pineapple', 'Ananas comosus'),
+        ('mango', 'Mangifera indica'),
+        ('blueberry', 'Vaccinium corymbosum'),
+        ('peach', 'Prunus persica'),
+        ('watermelon', 'Citrullus lanatus'),
+        ('cherry', 'Prunus avium'),
+        ('pear', 'Pyrus'),
+        ('plum', 'Prunus domestica'),
+        ('raspberry', 'Rubus idaeus'),
+        ('kiwi', 'Actinidia deliciosa'),
+        ('lemon', 'Citrus limon'),
+        ('avocado', 'Persea americana'),
+        ('pomegranate', 'Punica granatum'),
+        ('cranberry', 'Vaccinium macrocarpon'),
+        ('grapefruit', 'Citrus × paradisi')
+    ])
+    def test_good_key(self, good_key):
+        # setup
+        # disambiguate3
+        this_key, expected_value = good_key
 
+        # execute
+        #assert isinstance(get_formal_name(this_key), str)#pass if it returns string. error will be key error
+        #change structure of paramaterization list so that the value it returns is correct
+        assert get_formal_name(this_key) == expected_value
 
 class TestUnhappyPath(object):
-    def test_not_fruit(self):
-        assert not get_formal_name('bingus') #returns KeyError
+
+    @pytest.mark.parametrize('key_probs', [
+        'bingus',
+        'Apple',
+        ' apple',
+        'apple ',
+        'appple',
+        1,
+        #[1, 2, 3, 'bingus'],
+        True
+        ])
+
+    def test_key_error(self, key_probs):
+        #assert not get_formal_name('bingus') #returns KeyError
+        #assert not isinstance(get_formal_name("bingus"), str)
+        with pytest.raises(KeyError):
+            """
+            assert get_formal_name('bingus')
+            assert get_formal_name(1)
+            assert get_formal_name([1,2,3,'bingus'])
+            assert get_formal_name((1,2,3))
+            assert get_formal_name(true)
+            """
+            assert get_formal_name(key_probs)
+
+    @pytest.mark.parametrize('type_probs', [
+        [1, 2, 3, 'bingus'],
+    ])
+    def test_type_error(self, type_probs):
+        # assert not get_formal_name('bingus') #returns KeyError
+        # assert not isinstance(get_formal_name("bingus"), str)
+        with pytest.raises(TypeError):
+            assert get_formal_name(type_probs)
+
+    def test_no_args(self):
+        with pytest.raises(TypeError):
+            get_formal_name()
+
+
+    def test_double_args(self):
+        with pytest.raises(TypeError):
+            get_formal_name('apple', 'apple')
