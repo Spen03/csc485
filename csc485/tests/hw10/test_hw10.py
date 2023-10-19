ad2932adf14a656bb3f00eb7b131b9024e155169
Spen03 <jensenspencer00@gmail.com>
2023-10-18 22:43:43 -0400

    add homework10
    csc485\projects\tests\hw10\test_hw10.py
    +added files
    +formatted with flake8

--- csc485/tests/hw10/test_hw10.py
+++ csc485/tests/hw10/test_hw10.py
@@ -1,10 +1,8 @@
 import pytest
 from csc485.projects.hw10.fruit_query import is_it_a_fruit
 
+# test if it returns true or false for different inputs
 
-"""
-test if it returns true or false for different inputs
-"""
 
 """
 class TestHappyPath(object):
@@ -20,24 +18,32 @@ class TestHappyPath(object):
     def test_grape(self):
         assert is_it_a_fruit('grape')
 """
-#this runs through the keys______________________________
+
+
+# this runs through the keys______________________________
+
+
 @pytest.mark.parametrize('key', [
     'apple', 'pear', 'banana', 'grape'
 ])
 def test_good_key(key):
-    #setup
-    #disambiguate
+    # setup
+    # disambiguate
     this_key = key
 
-    #execute
+    # execute
     assert is_it_a_fruit(this_key)
-#______________________________________________
+
+
+# ______________________________________________
 
 """
 class TestUnhappyPath(object):
     def test_not_fruit(self):
         assert not is_it_a_fruit('bingus')
 """
+
+
 def test_int():
     assert not is_it_a_fruit(6)
     """
@@ -48,16 +54,18 @@ def test_int():
         assert not is_it_a_fruit(' grape')
     """
 
+
 @pytest.mark.parametrize('key', [
     'bingus', '6', 'Banana', ' grape'
 ])
 def test_bad_key(key):
-    #setup
-    #disambiguate
+    # setup
+    # disambiguate
     this_key = key
 
-    #execute
+    # execute
     assert not is_it_a_fruit(this_key)
+
 
 class TestNotString(object):
     def test_int(self):
