15e57b4a5ad804a9a0967633fbdc265ef5e3778f
Spen03 <jensenspencer00@gmail.com>
2023-10-24 13:52:44 -0400

    add homework14
    csc485\tests\hw14\test_api.py
    +move

--- csc485/tests/hw14/test_api.py
+++ csc485/tests/hw14/test_api.py
@@ -1,7 +1,8 @@
 import json
 import pytest
-#from path-to-the-api-code.api import app  # this is the flask app
+# from path-to-the-api-code.api import app  # this is the flask app
 from csc485.projects.hw14.api import app
+
 
 # this is a pytest fixture
 @pytest.fixture
@@ -11,7 +12,7 @@ def client():
         yield client
 
 
-# this is a simplified happy path test
+# this is a simplified happy path hw14
 def test_expect_good(client):
     # call the API, get the server response
     response = client.get('/get_strength?password=%%%%%%%')
@@ -20,11 +21,11 @@ def test_expect_good(client):
     # convert the json payload to a dict
     data = json.loads(response.data.decode())
 
-	assert data.get('password') == '%%%%%%%'
-	assert data.get('strength') == 'good'
+    assert data.get('password') == '%%%%%%%'
+    assert data.get('strength') == 'good'
 
 
-# this is an example very negative test
+# this is an example very negative hw14
 def test_api_error(client):
     # disambiguate
     password = '#password'
