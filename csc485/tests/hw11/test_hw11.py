2cc7eb016514054faba34a13e664e24e4d1823e5
Spen03 <jensenspencer00@gmail.com>
2023-10-25 23:14:12 -0400

    add homework12 revisions
    csc485\tests\hw11\test_hw11.py
    +added changes
    +formatted with flake8

--- csc485/tests/hw11/test_hw11.py
+++ csc485/tests/hw11/test_hw11.py
@@ -1,10 +1,9 @@
-#from csc485.data.fruit import fruit_dict
+# from csc485.data.fruit import fruit_dict
 import pytest
 
 from csc485.projects.hw11.get_fruit_name import get_formal_name
 
 # from csc485.projects.hw10.fruit_query import is_it_a_fruit
-
 # from csc485.projects.hw11.get_fruit_name_v2 import get_formal_name_v2
 # can't seem to import the new file for testing. Maybe I should make a new hw14 module?
 
@@ -15,7 +14,7 @@ it will return the correctly associated value
 hw14 that if it recieves an unknown key that it will return a key error
 these keys include:
 >strings
->ints 
+>ints
 >incorrectly formated strings
 >more than one argument
 >no argument
@@ -53,8 +52,10 @@ class TestHappyPath(object):
         this_key, expected_value = good_key
 
         # execute
-        # assert isinstance(get_formal_name(this_key), str)#pass if it returns string. error will be key error
-        # change structure of paramaterization list so that the value it returns is correct
+        # assert isinstance(get_formal_name(this_key), str)
+        # #pass if it returns string. error will be key error
+        # change structure of paramaterization-
+        # -list so that the value it returns is correct
         assert get_formal_name(this_key) == expected_value
 
 
@@ -87,8 +88,6 @@ class TestUnhappyPath(object):
         [1, 2, 3, 'bingus'],
     ])
     def test_type_error(self, type_probs):
-        # assert not get_formal_name('bingus') #returns KeyError
-        # assert not isinstance(get_formal_name("bingus"), str)
         with pytest.raises(TypeError):
             assert get_formal_name(type_probs)
 
