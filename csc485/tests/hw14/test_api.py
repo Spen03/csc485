e57db91e098cef3ae6a59f434f92ce2cefe624bc
Spen03 <jensenspencer00@gmail.com>
2023-11-12 18:11:27 -0500

    add homework14
    csc485\tests\hw14\test_api.py
    +added changes
    +formatted with flake8

--- csc485/tests/hw14/test_api.py
+++ csc485/tests/hw14/test_api.py
@@ -3,6 +3,10 @@ import pytest
 # from path-to-the-api-code.api import app  # this is the flask app
 from csc485.projects.hw14.api import app
 
+"""
+*
+"""
+
 
 # this is a pytest fixture
 @pytest.fixture
@@ -32,3 +36,45 @@ def test_api_error(client):
 
     with pytest.raises(ZeroDivisionError):
         response = client.get(f"/get_strength?password={password}")
+        assert response.status_code == 200
+
+
+@pytest.mark.parametrize('call_response', {
+    ('a' * 100 + '%' * 0, 'bad'),
+    ('a' * 99 + '%' * 1, 'bad'),
+    ('a' * 90 + '%' * 10, 'bad'),
+    ('a' * 51 + '%' * 49, 'bad'),
+    ('a' * 50 + '%' * 50, 'bad'),
+    ('a' * 49 + '%' * 51, 'good'),
+    ('a' * 1 + '%' * 99, 'good'),
+    ('a' * 0 + '%' * 100, 'good')
+})
+def test_paramaterize_call_response(client, call_response):
+    password, expected_strength = call_response
+    response = client.get('/get_strength?password=' + password)
+    assert response.status_code == 200
+
+    # convert the json payload to a dict
+    data = json.loads(response.data.decode())
+
+    # assert not len(data.get('password')) == len(password)
+
+    assert data.get('password') == password
+    assert data.get('strength') == expected_strength
+
+
+@pytest.mark.parametrize('bad_value_password', {
+    ('%%%%%%%%abc', 'good'),  # bad because of escape ...%%abc... => ...%%ABcabc...
+})
+def test_escape_inequal(client, bad_value_password):
+    password, expected_strength = bad_value_password
+    response = client.get('/get_strength?password=' + password)
+    assert response.status_code == 200
+
+    # convert the json payload to a dict
+    data = json.loads(response.data.decode())
+
+    assert len(data.get('password')) == len(password)  # why is this working now?
+
+    # assert data.get('password') == password
+    assert data.get('strength') == expected_strength
