5d0147df81516d801ff627d22b275c97ae147423
Spen03 <jensenspencer00@gmail.com>
2023-11-17 14:11:36 -0500

    add hw18: add setup.py
    csc485\setup.py
    
    +added changes
    +formatted with flake8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='csc485',
+   version='1.0',
+   description='code work for the class CSC 485',
+   author='Spencer Jensen',
+   packages=['csc485'],
+)
